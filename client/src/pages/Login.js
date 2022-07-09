ba42a9560df5fe56a514b4fd57fd948ac1ef717f
Caitlin Swickard <caitlin6029@gmail.com>
2022-07-09 15:17:22 -0600

    created basic register/login forms with no backend

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import './login.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Link } from 'react-router-dom'
 
 
 function Login() {
   return (
     <>
-      <h1>Login page</h1>
+      <section className="login">
+        <h1>Login</h1>
+        <Box
+          className='form'
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField id="outlined-basic" label="Username:" variant="outlined" />
+          <TextField id="outlined-basic" label="Email:" variant="outlined" />
+          <TextField id="outlined-basic" label="Password:" variant="outlined" />
+        </Box>
+        <p>
+          Not a Member?<br />
+          <span className="line">
+          <Link to="/register"> Register </Link>
+          </span>
+        </p>
+      </section>
     </>
   )
 }
